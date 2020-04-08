df8d664d29c4c4560655102b16c8d38f6a5c5187
RychuKapucino <rychukapucino@o2.pl>
2020-04-08 21:36:31 +0200

    Add menu

---
+++ Talia Zwyciestwa/Talia Zwyciestwa/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Talia_Zwyciestwa
+{
+	public partial class Form1 : Form
+	{
+		private bool isCollapsed;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (isCollapsed)
+			{
+				panelDropDown.Height += 10;
+				if (panelDropDown.Size == panelDropDown.MaximumSize)
+				{
+					timer1.Stop();
+					isCollapsed = false;
+				}
+			}
+			else
+			{
+				panelDropDown.Height -= 10;
+				if (panelDropDown.Size == panelDropDown.MinimumSize)
+				{
+					timer1.Stop();
+					isCollapsed = true;
+				}
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			timer1.Start();
+		}
+	}
+}
